7f18d67d27a74175d316b834aad348d4c92956ef
Tavis <71576767+TavisMac1@users.noreply.github.com>
2021-12-24 09:55:30 -0400

    hihat, kick drum and snare all work as expected. Audio will stop and create a new object and play as keypresses are detected making audio responsive to keypresses

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div class="jumbotron jumbotron-fluid">
         <div class=".container-fluid">
           <h1 class="display-4 .text-white">Tavis Drum App</h1>
-          <p class="lead .text-light">Press S, D or F to play sounds</p>
+          <p class="lead .text-light">Press S, D or C to play sounds</p>
         </div>
       </div>
       <br style={{paddingTop: 1000}} />
--- src/Display.js
+++ src/Display.js
@@ -5,12 +5,35 @@ import './App.css';
 
 const Display = () => {
 
-    let audio = new Audio("/Shoot.wav");
+    let snare = new Audio("/snare.mp3");
+    let hiHat = new Audio("/hihat.mp3");
+    let kick = new Audio("/kick.mp3");
 
     const playSound = (event) => {
-        if (event.key === 's') {
-            audio.play();
-            console.log('snare shouold go off');
+        if (event.key === 'd' || event.key === 'D' ) {
+            snare.play();
+
+            if (event.key === 'd' || event.key === 'D') {
+                snare.pause();
+                let snare2 = new Audio("/snare.mp3");
+                snare2.play();
+            }
+
+        } else if (event.key === 's' || event.key === 'S') {
+            hiHat.play();
+
+            if (event.key === 's' || event.key === 'S') {
+                hiHat.pause();
+                let hiHat2 = new Audio("/hihat.mp3");
+                hiHat2.play();
+             }
+        } else if (event.key === 'c' || event.key === 'C') {
+            kick.play();
+            if (event.key === 'c' || event.key === 'C') {
+                kick.pause();
+                let kick2 = new Audio("/kick.mp3");
+                kick2.play();
+             }
         }
     }
 
@@ -19,12 +42,11 @@ const Display = () => {
            <input type="text" className="input" autoFocus onKeyDown={playSound}/>
            <div>
                <h1 className='displayChar'>
-                   {}
+
                </h1>
            </div>
         </div>
     );
 } 
-
 
 export default Display;
